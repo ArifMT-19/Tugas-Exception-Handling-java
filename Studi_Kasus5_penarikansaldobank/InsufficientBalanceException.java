65c14608ede090b2f279b5aae7bbc48263a36336
Axcelion017 <michaelnewton1708@gmail.com>
2026-05-24 21:58:48 +0700

    feat:InsufficientBalanceException

--- Studi_Kasus5_penarikansaldobank/InsufficientBalanceException.java
+++ Studi_Kasus5_penarikansaldobank/InsufficientBalanceException.java
@@ -3,13 +3,13 @@ package Studi_Kasus5_penarikansaldobank;
 public class InsufficientBalanceException {
     public static void main(String[] args) {
         double balance = 200000.0; // Saldo awal
-        double withdrawalAmount = 500000.0; // Jumlah penarikan
+        double withdrawalAmounts = 500000.0; // Jumlah penarikan
 
         try{
-            if(withdrawalAmount > balance){
+            if(withdrawalAmounts > balance){
                 throw new Exception("Saldo tidak CUKUP");
             } else {
-                balance -= withdrawalAmount;
+                balance -= withdrawalAmounts;
                 System.out.println("Penarikan berhasil. Sisa saldo: " + balance);
             }
         } catch(Exception e){
